b47bd5d9fbdebd191028ed3303b50c1ad542c494
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2018-05-17 04:39:56 +0300

    Revert "nit"
    This reverts commit 96cea34b385a4df69a99810f9f1d67cdf5e653a0.

--- socks6msg_address.hh
+++ socks6msg_address.hh
@@ -44,20 +44,9 @@ public:
 		return type;
 	}
 	
-	in_addr getIPv4() const
-	{
-		return ipv4;
-	}
-	
-	in6_addr getIPv6() const
-	{
-		return ipv6;
-	}
-	
-	const boost::shared_ptr<std::string> getDomain() const
-	{
-		return domain.getStr();
-	}
+	in_addr getIPv4() const;
+	in6_addr getIPv6() const;
+	const boost::shared_ptr<std::string> getDomain() const;
 };
 
 }
