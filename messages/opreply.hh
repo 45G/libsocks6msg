2888b38c6ae4ca96a52408e8dcc036490c781c1a
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2020-01-23 19:53:56 +0200

    done with opreply

--- messages/opreply.hh
+++ messages/opreply.hh
@@ -1,14 +1,14 @@
 #ifndef SOCKS6MSG_OPREPLY_HH
 #define SOCKS6MSG_OPREPLY_HH
 
-#include "bytebuffer.hh"
+#include "socksmessagebase.hh"
 #include "address.hh"
 #include "optionset.hh"
 
 namespace S6M
 {
 
-struct OperationReply
+struct OperationReply: public SOCKSMessageBase<SOCKS6OperationReply>
 {
 	SOCKS6OperationReplyCode code;
 	
@@ -20,9 +20,27 @@ struct OperationReply
 	OperationReply(SOCKS6OperationReplyCode code, Address address = Address(), uint16_t port = 0)
 		: code(code), address(address), port(port) {}
 	
-	OperationReply(ByteBuffer *bb);
+	OperationReply(ByteBuffer *bb)
+		: SOCKSMessageBase(bb),
+		  code((SOCKS6OperationReplyCode)rawMessage->code),
+		  address((SOCKS6AddressType)rawMessage->addressType, bb),
+		  port(ntohs(rawMessage->bindPort)),
+		  options(bb, OptionSet::M_OP_REP, ntohs(rawMessage->optionsLength)) {}
 	
-	void pack(ByteBuffer *bb) const;
+	void pack(ByteBuffer *bb) const
+	{
+		SOCKS6OperationReply *rawOpReply = bb->get<SOCKS6OperationReply>();
+		
+		rawOpReply->version       = SOCKS6_VERSION;
+		rawOpReply->code          = code;
+		rawOpReply->optionsLength = htons(options.packedSize());
+		rawOpReply->bindPort      = htons(port);
+		rawOpReply->padding       = 0;
+		rawOpReply->addressType   = address.getType();
+		
+		address.pack(bb);
+		options.pack(bb);
+	}
 	
 	size_t pack(uint8_t *buf, size_t bufSize) const
 	{
