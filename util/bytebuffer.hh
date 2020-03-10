3d79692f455eea2fffa25e604fae896867d0575b
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2020-03-10 20:57:56 +0200

    nit

--- util/bytebuffer.hh
+++ util/bytebuffer.hh
@@ -18,9 +18,6 @@ public:
 	ByteBuffer(uint8_t *buf, size_t size)
 		: buf(buf), used(0), totalSize(size) {}
 	
-	ByteBuffer(const ByteBuffer &other)
-		: buf(other.buf), used(other.used), totalSize(other.totalSize) {}
-	
 	uint8_t *getBuf() const
 	{
 		return buf;
